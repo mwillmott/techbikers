ef93b19764751103973bf7702d62a459600bac38
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-05-20 11:29:54 +0100

    Add sponsor descriptions

--- client/rides/components/Sponsors.js
+++ client/rides/components/Sponsors.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Header = styled.h2`
   text-align: center;
   margin: 0;
-  margin-top: 10px;
+  margin-top: 20px;
 `;
 
 const Sponsors = styled.ul`
@@ -13,21 +13,45 @@ const Sponsors = styled.ul`
   justify-content: center;
   list-style-type: none;
   padding: 0;
-  margin: 0;
+  margin: 0 auto;
+  max-width: 800px;
 `;
 
-const Logo = styled.img`
+const Sponsor = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.a`
   display: block;
+  margin-right: 15px;
+  width: 200px;
+  flex-shrink: 0;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: ${props => props.height}px;
+
+  ${props =>
+    `background-image: url(${props.src})`
+  }
 `
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 16px;
+  line-height: 1.4;
+`;
 
 export default ({ sponsors, label, size }) => (
   <div>
     <Header>{label}</Header>
     <Sponsors>
       {sponsors.map(sponsor => (
-        <li sponsor={sponsor}>
-          <Logo src={sponsor.logo} height={size} />
-        </li>
+        <Sponsor sponsor={sponsor}>
+          <Logo href={sponsor.website} src={sponsor.logo} height={size}></Logo>
+          <Description>{sponsor.description}</Description>
+        </Sponsor>
       ))}
     </Sponsors>
   </div>
